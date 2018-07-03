d433342edef32402e2703e27633b9d3d918fc301
KHALIL Aref <aref.khalil@hexagon.com>
2018-07-03 12:00:50 +0200

    addning a class with change to branch2

---
+++ GirtTraining_DemoProject/branch2_class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GirtTraining_DemoProject
+{
+    class branch2_class
+    {
+        /// with changes 
+    }
+}
